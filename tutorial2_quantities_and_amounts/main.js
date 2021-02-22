11003416362182c90dce71a39558d7581757d784
evasilva1 <60716332+evasilva1@users.noreply.github.com>
2021-02-21 19:22:36 -0500

    load data and draw svg

---
+++ tutorial2_quantities_and_amounts/main.js
@@ -0,0 +1,27 @@
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
+.then(data => {
+    console.log("data", data)
+
+//constants
+const width = window.innerWidth*.8;
+const height =  window.innerHeight/3;
+//1.15
+    
+//SCALES
+//xscale - categorical, activity
+const xScale = d3.scaleBand()
+    .domain(data.map(d=> d.activity))
+    .range([0, width])
+//yscale - linear, count
+const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data,d=> d.count)])
+    .range([height, 0])
+//svg
+const svg = d3.select("#barchart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+//bars
+
+})
