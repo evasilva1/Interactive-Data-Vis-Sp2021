15ab819c772b99a3d05702e066085a4a901fc5b0
evasilva1 <60716332+evasilva1@users.noreply.github.com>
2021-03-01 09:03:39 -0500

    uploading new tutorial 3 code

--- tutorial3_distributions/NOTES.md
+++ tutorial3_distributions/NOTES.md
@@ -12,3 +12,11 @@ Sometimes it helps to formulate what you understood and where you got stuck in o
 If you find you're not able complete this week's assignment, reflecting on where you are getting stuck here will help you get full credit for this week's tutorial
 
 ------------
+I tried to get the transitions to work.
+
+I started with demo code
+
+I tried to get them to default to appear from margin.top and it did not work
+I tried to get them to appear from margin.right and it did not work
+
+I change enter transition, update transition and exit transition but it did not work
--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,23 +1,24 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
-  radius = 5;
+const width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8,
+  margin = { top: 30, bottom: 60, left: 60, right: 30 },
+  radius = 4;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
+let colorScale;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+  selectedBoro: "All" // + YOUR FILTER SELECTION
 };
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.csv("../data/Energy_Efficiency_Projects.csv", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("raw_data", raw_data);
   state.data = raw_data;
@@ -28,47 +29,132 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in 
 function init() {
   // + SCALES
+    xScale = d3.scaleLinear()
+      .domain(d3.extent(state.data, d=> d.NumberOfDays))
+      .range([margin.left,width - margin.right])
 
+    yScale = d3.scaleLinear()
+      .domain(d3.extent(state.data, d=> d.CO2e_Calculated_MT))
+      .range([height - margin.bottom, margin.top])
+
+    colorScale = d3.scaleOrdinal()
+      .domain(["Bronx","Brooklyn","Manhhattan","Queens","Staten Island"])
+      .range(["green","red","blue","pink","purple"])
   // + AXES
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale)
 
-  // + UI ELEMENT SETUP
+// + CREATE SVG ELEMENT
+  svg = d3.select("#d3-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
 
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
+// + CALL AXES
+ const xAxisGroup = svg.append("g")
+  .attr("class","xAxis")
+  .attr("transform",`translate(${0},${height - margin.bottom})`)
+  .call(xAxis)
 
-    state.selection = this.value
-    console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
-  });
+ xAxisGroup.append("text")
+  .attr("class",'axis-title')
+  .attr("x",width/2)
+  .attr("y",40)
+  .attr("text-anchor","middle")
+  .attr("font-size","12")
+  .attr("fill","black")
+  .text("Project Duration in Days")
+  
+ const yAxisGroup = svg.append("g")
+  .attr("class","yAxis")
+  .attr("transform",`translate(${margin.left},${0})`)
+  .call(yAxis)
 
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
-    .join("option")
-    .attr("value", d => d)
-    .text(d => d);
+ yAxisGroup.append("text")
+  .attr("class",'axis-title')
+  .attr("x",-40)
+  .attr("y",height/2)
+  .attr("writing-mode","vertical-lr")
+  .attr("text-anchor","middle")
+  .attr("fill","black")
+  .attr("font-size","12")
+  .text("CO2 Reduction in Metric Tons")
 
-  // + CREATE SVG ELEMENT
+// + SETUP UI ELEMENTS
+ const dropdown = d3.select("#dropdown")
 
-  // + CALL AXES
+ dropdown.selectAll("options")
+  .data([
+    { key: "All", label: "All"},
+    { key: "BRONX", label: "Bronx"},
+    { key: "BROOKLYN", label: "Brooklyn"},
+    { key: "MANHATTAN", label: "Manhattan"},
+    { key: "QUEENS", label: "Queens"},
+    { key: "STATEN IS", label: "Staten Island"}])
+  .join("option")
+  .attr("value", d => d.key)
+  .text(d => d.label)
 
+// + SET UP EVENT LISTENER
+dropdown.on("change", event=>{
+  console.log("dropdown change", event.target.value)
+  state.selectedBoro = event.target.value
+  console.log("new state", state)
+  draw();
+})
   draw(); // calls the draw function
 }
 
 /* DRAW FUNCTION */
  // we call this everytime there is an update to the data/state
 function draw() {
-  
-  // + FILTER DATA BASED ON STATE
+  console.log("in the drawing function")
 
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
+  const filteredData = state.data
+    .filter(d => {
+      if (state.selectedBoro === "All") return true
+      else return d.Borough === state.selectedBoro
+    })
+  
+  svg.selectAll("circle")
+    .data(filteredData, d => d.ID)
+    .join(
+      enter => enter.append("circle")
+       .attr("r",radius*1.5)
+       .attr("fill", d => colorScale(d.Borough))
+       // set attr before
+       .attr("cx", 0)
+       // start transition
+       .attr("cy", d => yScale(d.CO2e_Calculated_MT))
+       .call(enter=> enter.transition()
+     //.ease(d3.easeLinear)
+        .duration(1000))
+       // set end attr
+        .attr("cx", d => xScale(d.NumberOfDays))
+      //.attr("cy", d => yScale(d.CO2e_Calculated_MT))
+        ),
+
+      update => update
+        .call(sel => sel
+          .transition()
+          .duration(500)
+          .attr("r", radius*1.5)
+          .transition()
+          .duration(500)
+          .attr("r",radius)
+          ),
+      exit => exit
+       .call(sel => sel
+          .attr("opacity", 1)
+          .transition()
+          .duration(1000)
+          //.attr("cy", height)
+          .attr("opacity",0)
+          .remove()
+         )
+    
+      
+      //.attr("cx", d => xScale(d.NumberOfDays))
+       
+
 }
