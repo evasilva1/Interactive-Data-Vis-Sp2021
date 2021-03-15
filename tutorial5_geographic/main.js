69e440373e3c6ac7ad05c7e8763a3887d2907b8e
evasilva1 <60716332+evasilva1@users.noreply.github.com>
2021-03-14 23:57:13 -0400

    upload

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -1,9 +1,9 @@
 /**
  * CONSTANTS AND GLOBALS
  * */
-const width = window.innerWidth * 0.9,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8,
+  margin = { top: 30, bottom: 60, left: 60, right: 30 };
 
 /** these variables allow us to access anything we manipulate in
  * init() but need access to in draw().
@@ -15,6 +15,15 @@ let svg;
  * */
 let state = {
   // + SET UP STATE
+  geojson: null,
+  permit: null,
+  hover: {
+    stateName: null,
+    tempChange: null,
+    screenPosition: null,
+    mapPosition: null,
+    visible: false,
+  }
 };
 
 /**
@@ -22,10 +31,12 @@ let state = {
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("PATH_TO_YOUR_GEOJSON"),
-  d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
+  d3.json("../data/usState.json"),
+  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
 ]).then(([geojson, otherData]) => {
   // + SET STATE WITH DATA
+  state.temp = otherData
+  state.geojson = geojson
   console.log("state: ", state);
   init();
 });
@@ -35,6 +46,15 @@ Promise.all([
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
+  // long-lat -> x-y
+  const projection = d3.geoAlbersUsa()
+    .fitSize([width, height], state.geojson)
+
+
+  const pathFunction = d3.geoPath(projection)
+
+  //const graduateCenterCoords = [{long: -73.98, lat:40.7486}]
+
   // create an svg element in our main `d3-container` element
   svg = d3
     .select("#d3-container")
@@ -42,10 +62,58 @@ function init() {
     .attr("width", width)
     .attr("height", height);
 
-  // + SET UP PROJECTION
-  // + SET UP GEOPATH
-
   // + DRAW BASE MAP PATH
+  const states = svg.selectAll("path")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("stroke","black")
+    .attr("fill","#faf0e6")
+    .attr("opacity",".5")
+    .attr("d",pathFunction)
+
+
+  // + POINTS
+  svg.selectAll("circle.point")
+    .data(state.temp)
+    .join("circle")
+    .attr("r", 4)
+    .attr("stroke", "#ccc")
+    .attr("fill", d=> {
+      if (d.Change > 0) return "#E60026";
+      else if (d.Change === 0) return "#FFF880"
+      else return "005AD9"
+    })
+    .attr("fill-opacity", 0.5)
+    .attr("transform",d =>{
+      const [x,y] = projection([d.Long,d.Lat])
+      return `translate(${x},${y})`
+    })
+    .on("mousemove", function(event, d){
+      d3.select(this).transition()
+        .duration("50")
+        .attr("stroke","black")
+        .attr("r",16)
+      const {clientX, clientY} = event
+      const [long, lat] = projection.invert([clientX, clientY])
+
+      state.hover = {
+        stateName: d.State,
+        tempChange: d.Change,
+        screenPosition: [clientX, clientY],
+        mapPosition: [d3.format(".2f")(long), d3.format(".2f")(lat)],
+        visible: true
+      }
+      draw();
+    })
+    .on("mouseout", function(event,d){
+      d3.select(this).transition()
+        .duration("50")
+        .attr("stroke","#ccc")
+        .attr("r", 3)
+      state.hover.visible = false
+      draw();
+    })
+
   // + ADD EVENT LISTENERS (if you want)
 
   draw(); // calls the draw function
@@ -55,4 +123,23 @@ function init() {
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
-function draw() {}
+function draw() {
+  d3.select("#d3-container")
+    .selectAll('div.hover-content')
+    .data([state.hover])
+    .join("div")
+    .attr("class", 'hover-content')
+    .classed("visible", d => d.visible)
+    .style("position", 'absolute')
+    .style("transform", d => {
+      // only move if we have a value for screenPosition
+      if (d.screenPosition)
+        return `translate(${d.screenPosition[0]}px, ${d.screenPosition[1]}px)`
+    })
+    .html(d => {
+      return `
+      <div> State: ${d.stateName} </div>
+      <div> Coordinates: ${d.mapPosition} </div>
+      <div> Temperature changes in 95 days: ${d3.format(".2f")(d.tempChange)} </div>
+      `})
+}
