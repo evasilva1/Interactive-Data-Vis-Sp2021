8333ca54863f59eaf2ef912a1b2d4fd8640c05f3
evasilva1 <60716332+evasilva1@users.noreply.github.com>
2021-03-22 18:37:25 -0400

    successfully merge git

--- tutorial6_hierarchical/main.js
+++ tutorial6_hierarchical/main.js
@@ -39,8 +39,7 @@ function init() {
   svg = container
     .append("svg")
     .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [0,0,width,height]);
+    .attr("height", height);
 
 
   // + INITIALIZE TOOLTIP IN YOUR CONTAINER ELEMENT
