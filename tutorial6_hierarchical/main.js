a28d021c63d08af9e1e1ded7292f45a7237166bd
evasilva1 <60716332+evasilva1@users.noreply.github.com>
2021-03-29 20:36:13 -0400

    second try

--- tutorial6_hierarchical/main.js
+++ tutorial6_hierarchical/main.js
@@ -13,8 +13,8 @@ let tooltip;
  * */
 let state = {
   // + INITIALIZE STATE
-  //data: null,
-  //hover: null
+  data:null,
+  hover:null
 };
 
 /**
@@ -58,54 +58,55 @@ function init() {
 
   
   // + CREATE YOUR LAYOUT GENERATOR
-  const treeLayout = d3.pack()
+  const pack = d3.pack()
     .size([width, height])
     .padding(1)
     //.paddingInner(1)
     //.round(true)
   
   // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
-  //const tree = 
-  treeLayout(root)
-  const leaves = root.leaves()
+  pack(root)
+  const node = root.descendants()
 
   // + CREATE YOUR GRAPHICAL ELEMENTS
-  const leafGroup = svg.selectAll("g")
-      .data(leaves)
+  const packGroup = svg.selectAll("g")
+      .data(node)
       .join("g")
       //.attr("class", "leaf")
       .attr("transform", d => `translate(${d.x},${d.y})`)
   
   // + DRAW LEAVES RECT
-  leafGroup.append("circle")
+  packGroup.append("circle")
       //.attr("width", d => d.x1 - d.x0)
       //.attr("height", d => d.y1 - d.y0)
-      .attr("fill", d => {
+      .attr("fill", d => colorScale(d.height))//{
         // grab level
-        const level1Ancestor = d.ancestors().find(d => d.depth === 1 );
-        return colorScale(level1Ancestor.data.name)
-      })
+        //const level1Ancestor = d.ancestors().find(d => d.depth === 1 );
+        //return colorScale(level1Ancestor.data.name)
+      //})
       .attr("stroke","black")
       .attr("height", d => d.x)
       .attr("width", d => d.y)
       .attr("r", d => d.r)
   
-  leafGroup.append("text")
-      .text(d => d.data.name)
+  //packGroup.append("text")
+      //.text(d => d.data.name)
 
 
   // + ADD MOUSEOVER
-  leafGroup 
+  packGroup 
       .on("mouseenter", (event, d) => {
-        state.hoverleaf = d.data.name
-        state.hoverPositionX = d.x
-        state.hoverPositionY = d.y
+        state.hover = {
+          position: [d.x, d.y],
+          name: d.data.name,
+          value: d.data.value
+        }
         draw()
       })
-      //.on("mouseleave", () => {
-      //  state.hover = null
-      //  draw();
-      //})
+      .on("mouseleave", () => {
+        state.hover = null
+        draw();
+      })
 
   draw(); // calls the draw function
 }
@@ -116,9 +117,18 @@ function init() {
  * */
 function draw() {
   // + UPDATE TOOLTIP //working on getting the hover to work
-  tooltip
-    .style("transform", `translate(${state.hoverPositionX}px, ${state.hoverPositionY}px)`)
-    .html(`
-      <div></div>
-    `)
+  if (state.hover) {
+    tooltip
+    .html(
+      `
+      <div>Name: ${state.hover.name}</div>
+      <div>Value: ${state.hover.value}</div>
+      `
+    )
+    .style("font-size","12px")
+    .transition()
+    .duration(200)
+    .style("transform", `translate(${state.hover.position[0]}px,${state.hover.position[1]}px)`)
+  }
+  tooltip.classed("visible", state.hover)
 }
