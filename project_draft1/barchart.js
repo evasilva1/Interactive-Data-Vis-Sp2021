3692daf5f1e2cb2223fcb625ec9fe7507645ea3f
evasilva1 <60716332+evasilva1@users.noreply.github.com>
2021-04-12 15:55:20 -0400

    barchart update

--- project_draft1/barchart.js
+++ project_draft1/barchart.js
@@ -1,5 +1,5 @@
 // LOAD DATA
-d3.csv('../project_draft1/data/OLT_HPD.csv', d3.autoType)
+d3.csv('../project_draft1/data/OLT_HPD_CT.csv', d3.autoType)
 .then(data => {
     console.log("data", data)
 
@@ -8,34 +8,85 @@ const width = d3.select('#barchart').node().getBoundingClientRect().width;
 console.log("width",width);
 const height = d3.select('#barchart').node().getBoundingClientRect().height;
 console.log("height",height);
+margin = {top: 40, right:30, bottom:50, left:30}
 
-const nest = d3.rollup(data, v => v.length, d => d.Boro)
-console.log("nest",nest);
+//const nest = d3.rollup(data, v => v.length, d => d.Boro)
+//console.log("nest",nest);
 
 // SCALES
 // xscale - categorical, borough name
 const xScale = d3.scaleBand()     /* Need to figure out how to make it read 4 lables*/
-    .domain(data.map(d => d.Boro))
-    .range([0, width])
+    .domain(data.map(d => d.BOROUGH))
+    .range([margin.left, width- margin.right])
     .paddingInner(.2)
 
 // yscale - linear, count
 const yScale = d3.scaleLinear()
-    .domain([0, d3.max(nest, d => d.value)])
-    .range([height, 0])
+    .domain([d3.min(data, d => d.OLT), d3.max(data, d => d.OLT)])
+    .range([height - margin.bottom, margin.top])
 
 // SVG
 const svg = d3.select("#barchart")
     .append("svg")
-    .attr("width", 250)
-    .attr("height", 250)
+    .attr("width", width)
+    .attr("height", height)
+    .style('background-color', 'lightgrey');
 
 // BARS
 svg.selectAll("rect")
     .data(data)
     .join("rect")
     .attr("width", xScale.bandwidth())
-    .attr("height", d => yScale(d.value))
-    .attr("x", d=> xScale(d.Boro))
-    .attr("y", d=> yScale(d.value))
+    .attr("height", d => height - margin.bottom - yScale(d.OLT))
+    .attr("x", d=> xScale(d.BOROUGH))
+    .attr("y", d=> yScale(d.OLT))
+    .attr("fill","blue")
+    //.attr('x', (d,i) => i*(width/data.length))
+    //.attr('y', (d) => height-d*(height/100))
+    //.attr('width', 0.8*(width/data.length))
+
+
+ // + AXES
+ const xAxis = d3.axisBottom(xScale)
+ const yAxis = d3.axisLeft(yScale)
+
+// CALL AXES
+const xAxisGroup = svg.append("g")
+    .attr("class", 'xAxis')
+    .attr("transform", `translate(${0}, ${height - margin.bottom})`) // move to the bottom
+    .call(xAxis)
+
+const yAxisGroup = svg.append("g")
+.attr("class", 'yAxis')
+.attr("transform", `translate(${margin.left}, ${0})`) // align with left margin
+.call(yAxis)
+
+// add labels - xAxis
+xAxisGroup.append("text")
+.attr("class", 'axis-title')
+.attr("x", width / 2)
+.attr("y", 40)
+.attr("text-anchor", "middle")
+.text("Borough")
+
+// add labels - yAxis
+yAxisGroup.append("text")
+.attr("class", 'axis-title')
+.attr("x", -40)
+.attr("y", height / 2)
+.attr("writing-mode", "vertical-lr")
+.attr("text-anchor", "middle")
+.text("Environmental Score 2020")
+
+// Titles
+svg.append("text")
+    .attr("x", (width/2))
+    .attr("y", 20)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .style("font-weight", "bold")
+    .text("Old Law Tenements by Borough")
+
+
+
 })
--- project_draft1/timeline.js
+++ project_draft1/timeline.js
@@ -5,132 +5,74 @@ const height = d3.select('#timeline').node().getBoundingClientRect().height;
 console.log("height",height);
 radius = 4;
 
-// VARIABLES
-let svg;
-let xScale;
-let yScale;
-let yAxis;
-let xAxisGroup;
-let yAxisGroup;
-
-// APPLICATION STATE
-let state = {
-    data: [],
-    selectedPermit: "New Building",
-};
 
 // LOAD DATA
-d3.csv('../project_draft1/data/DummyPermit.csv', (d) => {
+d3.csv('../project_draft1/data/DummyPermitRev.csv', (d) => {
     return{
-        type: d.PermitType,
-        total: +d.Total,
+        totalt: +d.A1,
+        totdm: +d.DM,
+        totnb: +d.NB,
         year: new Date(d.Year,01,01) // (year, month, day)
     }
 })
     .then(data => {
         console.log("loaded data:", data);
-        state.data = data;
-        init();
-    });
-
-// INIT FUNCTIONS
-function init(){
-    // SCALES
-        xScale = d3.scaleTime()
-            .domain(d3.extent(state.data, d => d.year))
+    
+// SCALES
+        const xScale = d3.scaleTime()
+            .domain(d3.extent(data, d => d.year))
             .range([0,width])
         
-        yScale = d3.scaleLinear()
-            .domain(d3.extent(state.data, d => d.total))
-            .range([height, 0])
-        
-        colorScale = d3.scaleOrdinal(d3.schemePaired)
-    
-    // AXES
-        const xAxis = d3.axisBottom(xScale)
-        yAxist = d3.axisLeft(yScale)
+        const yScale = d3.scaleLinear()
+            .domain(d3.extent(data, d => d.total))
+            .range([height, 0])  
 
-    // SVG
+        colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+// AXIS
+        const xAxis = d3.axisBottom(xScale)
+        const yAxis = d3.axisLeft(yScale)
+
+// LINES
+        nLine = d3.line() // first line, nb
+            .x(d => xScale(d.year))
+            .y(d => yScale(d.totnb))
+        
+        dLine = d3.line() // second line, dm
+            .x(d => xScale(d.year))
+            .y(d => yScale(d.totdm))
+
+        aLine = d3.line() // third line, alt
+            .x(d => xScale(d.year))
+            .y(d => yScale(d.totalt))
+
+// SVG
         svg = d3.select('#timeline')
             .append("svg")
             .attr("width", width)
             .attr("height", height)
-    
-    // CALL AXES
-        const xAxisGroup = svg.append("g")
-            .attr("class","xAxis")
-            .attr("transform",`translate(${0},${height})`)
-            .call(xAxis)
+            .style('background-color', 'lightgrey')
+            .append("transform","translate(" + 20 +"," + 20 + ")");
+// LINES
+        svg.append("path")
+            .data(data)
+            .attr("class","line")
+            .style("stroke","red")
+            .attr("d", nLine);
 
-        xAxisGroup.append("text")
-            .attr("class", 'axis-title')
-            .attr("x", width/2)
-            .attr("y",40)
-            .attr("text-anchor", "middle")
-            .attr("font-size", "12")
-            .attr("fill", "black")
-            .text("Year")
+        svg.append("path")
+            .data(data)
+            .attr("class","line")
+            .style("stroke","blue")
+            .attr("d", dLine);
+
+        svg.append("path")
+            .data(data)
+            .attr("class","line")
+            .style("stroke","green")
+            .attr("d", aLine);
         
-        yAxisGroup = svg.append("g")
-            .attr("class", "yAxis")
-            .attr("transform", `translate(${0},${0})`)
-            .call(yAxis)
 
-        yAxisGroup.append("text")
-            .attr("class", 'axis-title')
-            .attr("x", -50)
-            .attr("y", height/2)
-            .attr("writing-mode", "vertical-lr")
-            .attr("text-anchor", "middle")
-            .attr("fill", "black")
-            .attr("font-size", "12")
-            .text("Total Number of Permits Issued")
+        
 
-    // UI ELEMENT SETUP
-        const dropdown = d3.select("#dropdown")
-
-        dropdown.selectAll("options")
-        .data(
-            Array.from(new Set(state.data.map(d => d.type)))
-        )
-        .join("option")
-        .attr("value", d => d)
-        .text(d => d)
-
-    // SET SELECT ELEMENT's DEFAULT VALUE
-            dropdown.on("change", event => {
-                console.log("dropdown changed to", event.target.value)
-                state.selectPermit = event.target.value
-                console.log("new state is", state)
-                draw();
-            })
-            draw();
-
-    // DRAW FUNCTION
-        function draw() {
-            console.log("in the drawing function")
-
-        // FILTER DATA
-        const filteredData = state.data
-        .filter(d => d.type === state.selectedPermit)
-
-        // UPDATE SCALE(S), if needed
-        yScale.domain([0, d3.max(filteredData, d => d.total)])
-
-        // UPDATE AXIS/AXES, if needed
-        yAxisGroup
-            .transition()
-            .duration(1000)
-            .call(yAxis.scale(yScale))
-
-        // DRAW CIRCLES 
-        const dots = svg
-            .selectAll(".dot")
-            .data(filteredDate, d => d.year)
-            .join(
-                enter => enter.append("g")
-                    .attr("class", )
-            )
-        }
-}
-   
+    });
